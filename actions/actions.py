f6a34f3f0b0dd0ec793e71ae2e84bf7518d9f6e0
Joe Diamand <jdiamand@yahoo.com>
2024-02-24 12:49:07 -0500

    Your input ->  hi Welcome to the COP6481 Branch of the FinTech World Bank Your userid has been verified,What would you like help with today?,Your input ->  account info,Great, let me pull up that information ...

--- actions/actions.py
+++ actions/actions.py
@@ -28,9 +28,10 @@
 
 
 from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action
 from rasa_sdk.executor import CollectingDispatcher
-
+from rasa_sdk.interfaces import Tracker
+from rasa_sdk.events import SlotSet
 class ActionSaveName(Action):
     def name(self) -> Text:
         return "action_save_name"
@@ -41,5 +42,36 @@ class ActionSaveName(Action):
         name = \
 	next(tracker.get_latest_entity_values("name"), None)
         dispatcher.utter_message(text=f"Hello, {name}!")
+        return []
+
+
+
+
+
+class ActionCustomGreet(Action):
+    def name(self) -> str:
+        return "action_custom_greet"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: dict) -> List[Dict[Text, Any]]:
+        greeted = tracker.get_slot("greeted")
+        if not greeted:
+            message = (
+                "Welcome to the COP6481 Branch of the FinTech World Bank\n\n"
+                "Your userid has been verified\n\n"
+                "What would you like help with today?\n\n"
+            )
+            dispatcher.utter_message(text=message)
+            return [SlotSet("greeted", True)]
+        else:
+            dispatcher.utter_message(text="Hi there, what can I do for you?")
+            return []
+
+
+class ActionShowAccountInfo(Action):
+    def name(self) -> str:
+        return "action_show_account_info"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(response="utter_account_info")
         return []
 
